a7cb8958a2f7664d101fd32fc8619fe4bd375bfb
tungnq <osbproject01@gmail.com>
2024-11-28 10:22:40 +0700

    Upload WebGL folder

---
+++ Assets/CursorManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    void Start()
+    {
+        // Hiển thị con trỏ chuột
+        Cursor.visible = true;
+
+        // Đảm bảo con trỏ không bị khóa
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        // Đảm bảo con trỏ luôn hiển thị và không bị khóa trong suốt quá trình chạy game
+        if (!Cursor.visible || Cursor.lockState != CursorLockMode.None)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
---
+++ Assets/OpenLinks.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class OpenLinks : MonoBehaviour
+{
+    public string url = "https://vrbizos.io.vn/";
+
+    void Update()
+    {
+        // Kiểm tra nếu người dùng nhấn phím R
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            OpenURL(url);
+        }
+    }
+
+    public static void OpenURL(string url)
+    {
+#if !UNITY_EDITOR && UNITY_WEBGL
+        // Sử dụng ExternalCall thay vì ExternalEval
+        Application.ExternalCall("openUrlInNewTab", url);
+#else
+        Application.OpenURL(url);
+#endif
+    }
+}
